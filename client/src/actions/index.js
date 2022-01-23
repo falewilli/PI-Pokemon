1ec6fb8f1233c66f86a3649ee0752e573cba5077
falewilli <alejandro.willi@gmail.com>
2022-01-23 16:55:51 -0300

    First commit new repo

---
+++ client/src/actions/index.js
@@ -0,0 +1,111 @@
+import axios from 'axios';
+
+export function getPokemons(){
+    return async function(dispatch){
+        var json= await axios.get('http://localhost:3001/pokemons');
+        return dispatch({
+            type: 'GET_POKEMONS',
+            payload: json.data
+        })
+    }
+}
+
+export function getNamePokemons(name){
+    return async function (dispatch){
+        try {
+            // var json = await axios.get(`http://localhost:3001/pokemons?name=${name}`);
+            var json = await axios.get('http://localhost:3001/pokemons?name=' + name);
+            return dispatch ({
+                type: 'GET_NAME_POKEMONS',
+                payload: json.data
+            })
+        } catch (error) {
+            return dispatch ({
+                type: 'GET_NAME_POKEMONS',
+                payload: null
+            })
+            // console.log(error)
+        }
+    }
+}
+
+export function filteredByLife(payload){
+    // console.log(payload)
+    return{
+        type: 'GET_LIFE',
+        payload
+    }
+}
+
+export function filterCreated(payload){
+    // console.log(payload)
+    return{
+        type: 'FILTER_CREATED',
+        payload
+    }
+}
+
+export function orderByNameStrength(payload){
+    //console.log(payload)
+    return{
+        type: 'ORDER_BY_NAME_STRENGTH',
+        payload
+    }
+}
+
+// export function resetFilters(payload) {
+//     return {
+//       type: RESET_FILTERS,
+//       payload,
+//     };
+// }
+
+export function getTypes(){
+    return async function (dispatch) {
+        var json = await axios.get('http://localhost:3001/types');
+        return dispatch({
+            type: 'GET_TYPES',
+            payload: json.data
+        })
+    }
+}
+
+export function filterByTypes(payload){
+    //console.log(payload)
+    return {
+        type: 'FILTER_BY_TYPES',
+        payload
+    }
+}
+
+export function getDetail(id){
+    return async function (dispatch){
+        try{
+            var json = await axios.get('http://localhost:3001/pokemons/' + id);
+            return dispatch ({
+                type: 'GET_DETAIL',
+                payload: json.data
+            }) 
+        }catch(error){
+            return dispatch ({
+                type: 'GET_DETAIL',
+                payload: null
+            })
+            // console.log(error)
+        }
+    }
+}
+
+export function clearDetail() {
+    return {
+      type: 'CLEAR_DETAIL',
+    }
+}
+
+export function postPokemon(payload){
+    return async function (dispatch) {
+        var response = await axios.post('http://localhost:3001/pokemons', payload);
+        // console.log(response);
+        return response;
+    }
+}
